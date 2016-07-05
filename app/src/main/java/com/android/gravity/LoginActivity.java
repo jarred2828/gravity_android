1ae589091e536e5690836d5fdf4f701ebaad90d9
Justin Lee <j.lee1205@hotmail.com>
2016-07-05 16:03:52 +0800

    Created authentication page

--- app/src/main/java/com/android/gravity/LoginActivity.java
+++ app/src/main/java/com/android/gravity/LoginActivity.java
@@ -1,16 +1,55 @@
 package com.android.gravity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button fbLoginButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        fbLoginButton = (Button) findViewById(R.id.fbLoginButton);
+        fbLoginButton.setOnClickListener(this);
+
+        signUpButton = (Button) findViewById(R.id.signupButton);
+        signUpButton.setOnClickListener(this);
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+
+        switch (v.getId()) {
+            case R.id.fbLoginButton:
+                //@Todo: Press facebook Log in button
+                startActivity(intent);
+                break;
+            case R.id.signupButton:
+                //@Todo: Press sign up button
+                startActivity(intent);
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+    }
+
+        // Disable back button
+    @Override
+    public void onBackPressed()
+    {
+        return;
     }
 }
--- app/src/main/java/com/android/gravity/MainActivity.java
+++ app/src/main/java/com/android/gravity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.android.gravity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int REQUEST_ACCESS_FINE_LOCATION_PERMISSION_RESULT = 1;
+    private static int REQUEST_CONTACT_PERMISSION_RESULT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +19,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
 
-    // Disable back button
-    @Override
-    public void onBackPressed()
-    {
-        return;
+        // request location permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                //@Todo: get location info
+            } else {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    //@Todo: nothing
+                }
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION_PERMISSION_RESULT);
+            }
+        } else {
+            //@Todo: get location info
+        }
+
+        // request contact permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                //@Todo: get contact info
+            } else {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+                    //@Todo: nothing
+                }
+                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CONTACT_PERMISSION_RESULT);
+            }
+        } else {
+            //@Todo: get contact info
+        }
     }
 }
